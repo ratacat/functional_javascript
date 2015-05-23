68fe7677be11116108f4ebeba07ca89ae9327f06
ratacat <neuralsplash@gmail.com>
2015-05-23 14:43:26 -0700

    passed repeat

--- repeat.js
+++ repeat.js
@@ -1,17 +1,18 @@
-var REPL = require("repl");
+// var REPL = require("repl");
 
-var repl = REPL.start("func > ");
-repl.context.low = low;
+// var repl = REPL.start("func > ");
 
-repl.on("exit", function () {
-  console.log("GOODBYE!!");
-  process.exit();
-});
+// repl.on("exit", function () {
+//   console.log("GOODBYE!!");
+//   process.exit();
+// });
 
 
 function repeat(operation, num) {
-  
+  for(i=1;i<=num;i++){
+  	operation();
+  }
 }
 
 // Do not remove the line below
-module.exports = repeat
+module.exports = repeat;
