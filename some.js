00f6be715a71c063403f8036979f65bd039fcaf6
ratacat <neuralsplash@gmail.com>
2015-05-24 08:14:02 -0700

    not done with some yet

--- some.js
+++ some.js
@@ -1,7 +1,23 @@
     function checkUsersValid(goodUsers) {
+    	//console.log("goodUsers: "+goodUsers);
+      	
       return function allUsersValid(submittedUsers) {
-        // SOLUTION GOES HERE
+      	for(i=0;i<submittedUsers.length;i++){
+      		console.log(i + ":" + submittedUsers[i] );
+      	}
+      	//console.log("submittedUsers: "+submittedUsers);
+      	// submittedUsers.every(function(subV,subI) {
+      	// 	console.log('subi: '+ subI);
+      	// 	goodUsers.some(function(goodV,goodI) {
+      	// 		console.log(subI + " " + goodI + " " + subV === goodV ? true : false);
+      	// 		return subV === goodV ? true : false;
+      	// 	});
+      	// });
       };
+
     }
     
     module.exports = checkUsersValid
+
+
+
